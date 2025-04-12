82964918c1033423e4cde3b462c734a3b72a6762
SieRaX <matthewcool@snu.ac.kr>
2025-04-12 12:15:20 +0000

    code for plotting kl divergence

---
+++ diffusion_policy/model/diffusion/kl_divergence.py
@@ -0,0 +1,44 @@
+import torch
+
+def normal_kl(mean1, logvar1, mean2, logvar2):
+    """
+    Compute the KL divergence between two gaussians.
+    Shapes are automatically broadcasted, so batches can be compared to
+    scalars, among other use cases.
+    """
+    tensor = None
+    for obj in (mean1, logvar1, mean2, logvar2):
+        if isinstance(obj, torch.Tensor):
+            tensor = obj
+            break
+    assert tensor is not None, "at least one argument must be a Tensor"
+
+    # Force variances to be Tensors. Broadcasting helps convert scalars to
+    # Tensors, but it does not work for torch.exp().
+    logvar1, logvar2 = [
+        x if isinstance(x, torch.Tensor) else torch.tensor(x).to(tensor)
+        for x in (logvar1, logvar2)
+    ]
+
+    kl = 0.5 * (
+        -1.0
+        + logvar2
+        - logvar1
+        + torch.exp(logvar1 - logvar2)
+        + ((mean1 - mean2) ** 2) * torch.exp(-logvar2)
+    )
+    kl = kl.mean(dim=[1,2])
+    # mean1_minus_mean2 = ((mean1 - mean2) ** 2).mean(dim=[1,2])
+    # mean1_minus_mean2 = torch.where(mean1_minus_mean2 > 1000, torch.zeros_like(mean1_minus_mean2), mean1_minus_mean2)
+    return 0.5 * (
+        -1.0
+        + logvar2
+        - logvar1
+        + torch.exp(logvar1 - logvar2)
+        + ((mean1 - mean2) ** 2) * torch.exp(-logvar2)
+    )
+    
+def extract(a, t, x_shape):
+    b, *_ = t.shape
+    out = a.gather(-1, t)
+    return out.reshape(b, *((1,) * (len(x_shape) - 1)))
